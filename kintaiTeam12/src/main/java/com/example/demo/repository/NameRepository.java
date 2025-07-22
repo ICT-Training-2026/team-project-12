c5c85121e8819b3ed993670a460a3f4d11018a54
github Tsukumo-Ohira <tsukumo-ohira@kccs.co.jp>
2025-07-22 15:43:26 +0900

    マイページの値取得の実装

--- kintaiTeam12/src/main/java/com/example/demo/repository/NameRepository.java
+++ kintaiTeam12/src/main/java/com/example/demo/repository/NameRepository.java
@@ -1,5 +1,7 @@
 package com.example.demo.repository;
 
+import java.time.LocalDate;
+
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
@@ -23,10 +25,34 @@ public class NameRepository {
 		return name;
 	}
 	
-	public int sumByHours() {
+	public String sumByHours(String empnum) {
 		
-		int total = 0;
+		String total = "0";
 		
+		
+		LocalDate currentDate = LocalDate.now();
+		
+		int year = currentDate.getYear();
+		int month = currentDate.getMonthValue();
+		
+		LocalDate firstDayOfMonth = LocalDate.of(year, month, 1);
+		LocalDate lastDayOfMonth = firstDayOfMonth.withDayOfMonth(firstDayOfMonth.lengthOfMonth());
+		
+		String sql = "SELECT "
+	            + "    SUM((FINISH_HOUR * 60 + FINISH_MINUTE) - (START_HOUR * 60 + START_MINUTE) - REST_TIME) / 60 AS WORK_HOURS "
+	            + "FROM "
+	            + "    ATTENDANCES "
+	            + "WHERE "
+	            + "    EMPLOYEE_NUM = ? "
+	            + "    AND ATTENDANCE_TYPE IN (?, ?, ?) " 
+	            + "    AND YEAR BETWEEN ? AND ? "
+	            + "GROUP BY "
+	            + "    EMPLOYEE_NUM ";
+		
+	    total = jdbcTemplate.queryForObject(sql, String.class, empnum, "出勤", "年休", "振出", firstDayOfMonth, lastDayOfMonth);
+		
+	    int dotIndex = total.indexOf(".");
+	    total = (dotIndex == -1) ? total : total.substring(0, dotIndex);
 		return total;
 	}
 }
