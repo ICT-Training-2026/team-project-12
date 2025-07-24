1977f3d4bd62e45787bb700462efdd51e6a9718c
github Tsukumo-Ohira <tsukumo-ohira@kccs.co.jp>
2025-07-24 13:04:36 +0900

    検索結果からマイページへの遷移機能の更新

--- kintaiTeam12/src/main/java/com/example/demo/controller/SpecialSearchResultController.java
+++ kintaiTeam12/src/main/java/com/example/demo/controller/SpecialSearchResultController.java
@@ -1,16 +1,34 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.example.demo.entity.MyPage;
 import com.example.demo.form.EmployeeNumForm;
+import com.example.demo.form.MyForm;
+import com.example.demo.service.NameService;
+
+import lombok.RequiredArgsConstructor;
 
 @Controller
+@RequiredArgsConstructor
 public class SpecialSearchResultController {
 	
+	private final NameService nservice;
+	
 	@PostMapping("/my_page_spresult")
-	public String returnSpecialSearchPage(@ModelAttribute EmployeeNumForm empnumform) {
+	public String returnSpecialSearchPage(@ModelAttribute EmployeeNumForm empform, @ModelAttribute MyForm myform,Model model) {
+		
+		
+		MyPage tmp = new MyPage();
+		
+		tmp = nservice.MyReference(empform.getEmployeeNum());
+		
+		model.addAttribute("employeenum",empform.getEmployeeNum());
+		model.addAttribute("name",tmp.getName());
+		model.addAttribute("hours",tmp.getHours());
 		
 		return "my-page";
 	}
