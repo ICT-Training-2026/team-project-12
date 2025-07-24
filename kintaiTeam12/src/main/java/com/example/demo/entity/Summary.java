9370c1a6827853a70d5347e3ac3d69b6dce706f8
github chinatsu-shibayama <chinatsu-shibayama@kccs.co.jp>
2025-07-24 16:12:37 +0900

    （entity）Summaryの作成,edit-deleteで使うよ

---
+++ kintaiTeam12/src/main/java/com/example/demo/entity/Summary.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import java.sql.Date;
+
+import lombok.Data;
+
+@Data
+public class Summary {
+	private String employeeNum;
+	
+	private Date date;
+	private String empName;
+	private String composeId;
+	private String attendanceType;
+	private String startTime;
+	private String finishTime;
+	private int restTime;
+}
