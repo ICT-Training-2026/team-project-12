381dbb4a9c33a583049c4f9eda8f71387b47029b
github Shunsuke-Nakano36 <shunsuke-nakano@kccs.co.jp>
2025-07-28 10:54:29 +0900

    コミット

--- kintaiTeam12/src/main/java/com/example/demo/service/EditConfirmService.java
+++ kintaiTeam12/src/main/java/com/example/demo/service/EditConfirmService.java
@@ -16,15 +16,32 @@ public class EditConfirmService {
 	public void editBridge(Summary summary) {
 		
 		System.out.println("EditConfirmService 起動確認：" + summary);
-		
-		
-		
-		
-		
-		
-		
-		
-		
+
+		if ("年休".equals(summary.getAttendanceType())) {
+
+			System.out.println("if年休：起動確認");
+
+			ecrepository.updateByPaidHoliday(summary);
+		}
+
+		// 振出の計算
+		if ("振出".equals(summary.getAttendanceType())) {
+
+			System.out.println("if振出：起動確認");
+
+			ecrepository.updateByPlusSubstitudeHoliday(summary);
+
+		}
+
+		// 振休の計算
+		if ("振休".equals(summary.getAttendanceType())) {
+
+			System.out.println("if振休：起動確認");
+
+			ecrepository.updateByMinusSubstitudeHoliday(summary);
+
+		}
+
 		// 勤怠時間の編集登録
 		ecrepository.updateByEdit(summary);
 		
